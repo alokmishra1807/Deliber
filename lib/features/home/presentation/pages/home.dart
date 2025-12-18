8b2e650a6cb328a1edfc470b03ce02368e5aa0b9
alokmishra1807 <alok.230101016@iiitbh.ac.in>
2025-12-19 02:57:51 +0530

    add bottom sheet

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,3 +1,5 @@
+import 'package:deliber/features/home/presentation/widget/bottomsheet.dart';
+import 'package:deliber/features/home/presentation/widget/sendparcel.dart';
 import 'package:deliber/features/location/presentation/bloc/location_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,45 +15,65 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => HomepageState();
 }
 
+
 class HomepageState extends State<Homepage> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
   static const CameraPosition _defaultCamera = CameraPosition(
-    target: LatLng(20.5937, 78.9629), // India center
+    target: LatLng(20.5937, 78.9629),
     zoom: 5,
   );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<LocationBloc, LocationState>(
-        listener: (context, state) async {
-          if (state is LocationGranted) {
-            final controller = await _controller.future;
-
-            await controller.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                  target: LatLng(
-                    state.location.latitude!,
-                    state.location.longitude!,
+      body: Stack(
+        children: [
+         
+          BlocListener<LocationBloc, LocationState>(
+            listener: (context, state) async {
+              if (state is LocationGranted) {
+                final controller = await _controller.future;
+                await controller.animateCamera(
+                  CameraUpdate.newCameraPosition(
+                    CameraPosition(
+                      target: LatLng(
+                        state.location.latitude!,
+                        state.location.longitude!,
+                      ),
+                      zoom: 15,
+                    ),
                   ),
-                  zoom: 15,
-                ),
-              ),
-            );
-          }
-        },
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _defaultCamera,
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-        ),
+                );
+              }
+            },
+            child: GoogleMap(
+              mapType: MapType.terrain,
+              initialCameraPosition: _defaultCamera,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              onMapCreated: (controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+
+          
+         Positioned.fill(
+  child: BottomSheetWidget(
+    onTap: () {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (_) => const Sendparcel(),
+      );
+    },
+  ),
+),
+
+        ],
       ),
     );
   }
---
+++ lib/features/home/presentation/widget/bottomsheet.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:flutter/material.dart';
+
+class BottomSheetWidget extends StatelessWidget {
+  final VoidCallback onTap;
+  const BottomSheetWidget({
+    Key? key,
+    required this.onTap
+    
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.45,
+      minChildSize: 0.45,
+      maxChildSize: 0.9,
+      builder: (context, scrollController) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: ListView(
+            controller: scrollController,
+            padding: EdgeInsets.zero,
+            children: [
+              const SizedBox(height: 8),
+
+              Center(
+                child: Container(
+                  width: 40,
+                  height: 4,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade400,
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 18),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(12),
+                  onTap: onTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 14,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 8,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.search_rounded, color: Colors.black),
+                        SizedBox(width: 12),
+                        Text(
+                          "Where do you want to go?",
+                          style: TextStyle(fontSize: 16, color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
